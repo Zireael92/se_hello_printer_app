747b5277716037c8cc5e91eb59cf6d94f56232e7
Zireael92 <krygula@gmail.com>
2021-04-25 09:52:42 +0000

    cwiczenia json

--- hello_world/views.py
+++ hello_world/views.py
@@ -1,18 +1,22 @@
 from hello_world import app
 from hello_world.formater import get_formatted
-from hello_world.formater import SUPPORTED, PLAIN
+from hello_world.formater import SUPPORTED, PLAIN, XML
 from flask import request
 
 moje_imie = "Karolina"
 msg = "Hello World!"
 
 
+
 @app.route('/')
 def index():
     output = request.args.get('output')
+    name = request.args.get('name')
     if not output:
         output = PLAIN
-    return get_formatted(msg, moje_imie,
+    if not name:
+        name = moje_imie
+    return get_formatted(msg, name,
                          output.lower())
 
 
